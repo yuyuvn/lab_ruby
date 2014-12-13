9a68b2ebb0917a1cb401238f0125d912a880d505
Clicia Scarlet <yuyuvn@mac.com>
2014-12-13 21:22:58 +0700

    Edit homepage & change signup url

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'about' => 'static_pages#about'
   
+  get 'signup' => 'users#new'
+  
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
