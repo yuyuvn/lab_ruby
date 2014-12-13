88e8a4d0102e2fc9e2b2760b973b77cad5e7898d
Clicia Scarlet <yuyuvn@mac.com>
2014-12-13 22:21:07 +0700

    login / logout / remember me

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,16 @@ class UsersController < ApplicationController
 	end
 	
 	def new
+		redirect_to root_url if logged_in?
 		@user = User.new
 	end
 	
 	def create
+		redirect_to root_url if logged_in?
 		@user = User.new(user_params)
 		if @user.save
+			log_in @user
+			remember user
 			flash[:success] = "Sign up successful!"
 			redirect_to @user
 		else
