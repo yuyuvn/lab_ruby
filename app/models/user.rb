08be595322615db7dfbe2f08ab4eb56b2fd4c215
Clicia Scarlet <yuyuvn@mac.com>
2014-12-13 20:43:26 +0700

    Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
-						format: { with: VALIDE=EMAIL=REGEX },
+						format: { with: VALID_EMAIL_REGEX },
 						uniqueness: { case_sensitive: false }
 						
 	has_secure_password
