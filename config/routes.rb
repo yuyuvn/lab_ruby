f7cb35dd958335e35358a5f1681d62e45b11fb5f
Clicia Scarlet <yuyuvn@mac.com>
2014-12-13 20:05:12 +0700

    User model

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'about' => 'static_pages#about'
+  
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
