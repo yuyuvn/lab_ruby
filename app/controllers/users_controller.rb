d2cc7abf8d21924988cc449bfe1ea7c661ec8bef
Clicia Scarlet <yuyuvn@mac.com>
2014-12-23 22:51:57 +0700

    Account activations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,9 @@ class UsersController < ApplicationController
 		redirect_to root_url if logged_in?
 		@user = User.new(user_params)
 		if @user.save
-			log_in @user
-			remember user
-			flash[:success] = "Sign up successful!"
-			redirect_to @user
+			@user.send_activation_email
+			flash[:info] = "Please check your email to activate your account."
+			redirect_to root_url
 		else
 			render 'new'
 		end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223133026) do
+ActiveRecord::Schema.define(version: 20141223153334) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20141223133026) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
+    t.string   "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
   end
 
 end
