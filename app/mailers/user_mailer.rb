1fd28651a7c33c72fb14bd9aca9189a988668188
Clicia Scarlet <yuyuvn@mac.com>
2014-12-23 23:01:32 +0700

    Password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,13 @@
 class UserMailer < ActionMailer::Base
-  default from: "noreply@sampleapp.com"
+  default from: "noreply@example.com"
 
   def account_activation(user)
     @user = user
     mail to: user.email, subject: "Account activation"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	
 	before_save :downcase_email
 	before_create :create_activation_digest
@@ -37,15 +37,6 @@ class User < ActiveRecord::Base
 		update_attribute(:remember_digest, nil)
 	end
 	
-	def downcase_email
-      self.email = email.downcase
-    end
-	
-	def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
-	
 	def activate
 		update_attribute(:activated,    true)
 		update_attribute(:activated_at, Time.zone.now)
@@ -54,4 +45,29 @@ class User < ActiveRecord::Base
 	def send_activation_email
 		UserMailer.account_activation(self).deliver_now
 	end
+	
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest,  User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
+	
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
+	end
+	
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
+	end
+  
+	private
+	
+	def downcase_email
+      self.email = email.downcase
+    end
+	
+	def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
